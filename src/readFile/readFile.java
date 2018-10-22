3c185d87e1bae5107cfcea35e04b743f74646a02
DinhNguyen <nguyendinh180299@gmail.com>
2018-10-23 00:09:42 +0700

    push code

--- src/readFile/readFile.java
+++ src/readFile/readFile.java
@@ -1,45 +1,53 @@
-package readFile;
-import sample.Word;
+package ReadFile;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+public class ReadFile {
+    private ArrayList<String> englishWordList = new ArrayList<String>();
+    private String[] meaningList;
 
-public class readFile {
-    public static void readFile() throws IOException{
-        String fileName = "D:\\anhviet109K.txt";
-        String content = new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
-        String [] word = content.split("\n\n");
+    /**
+     * Contructor
+     *
+     * @param englishWordList
+     */
+    public ReadFile(ArrayList<String> englishWordList) throws IOException {
+        this.englishWordList = englishWordList;
+    }
 
-        //System.out.println(word[3]);
+    public ReadFile() throws IOException {}
 
-        ArrayList<Word> anh = new ArrayList<Word>();
+    private String content = new String(Files.readAllBytes(Paths.get("anhviet.txt")), StandardCharsets.UTF_8);
+    private String[] str = content.split("\n\n");
 
-        for(int i=0; i<10; i++){
-            String [] b;
-            b = word[i].split("/");
-
-//            int size = b.length;
-//            String wordExplane = new String();
-//            for(int j=1; j<size; j++) {
-//                wordExplane += b[j].concat("\n");
-//            }
-//            Word word1 = new Word();
-//            word1.setWordTarget(b[0]);
-//            word1.setWordExplain(wordExplane);
-//            anh.add(word1);
-
+    public ArrayList<String> getEnglishWordList() {
+        for (String w : str) {
+            String[] a;
+            a = w.split("/");
+            englishWordList.add(removeCharAt(a[0], 0));
         }
-        System.out.println(anh.get(3).getWordTarget() + "\nNghia: " + anh.get(3).getWordExplain());
+        return englishWordList;
+    }
 
-}
-    public static void main(String[] args) throws IOException {
-        readFile();
+    public String[] getMeaningList() {
+        meaningList = str;
+        return meaningList.clone();
+    }
+
+    public static String removeCharAt(String s, int pos) {
+        return s.substring(0, pos) + s.substring(pos + 1);
+    }
+
+    public static void main(String[] args) {
+        try {
+            ReadFile file = new ReadFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
